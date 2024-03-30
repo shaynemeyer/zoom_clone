082d2965b59d331b08be16e57bd0467bf4ae766c
Shayne Meyer <shayne.meyer@gmail.com>
2024-03-30 08:10:38 -0700

    added home cards

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import MobileNav from "./MobileNav";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 function Navbar() {
   return (
@@ -19,7 +20,9 @@ function Navbar() {
         </p>
       </Link>
       <div className="flex-between gap-5">
-        {/*  Clerk User Managment */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
 
         <MobileNav />
       </div>
